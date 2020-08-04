140937c6f334ffbe6d7efae4a967b2c811f03fca
Crystal <crystal@crystalos.net>
2020-08-04 13:49:08 +0800

    创建项目

---
+++ src/main/java/net/crystalos/nacosservice/NacosServiceApplication.java
@@ -0,0 +1,13 @@
+package net.crystalos.nacosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServiceApplication.class, args);
+    }
+
+}
